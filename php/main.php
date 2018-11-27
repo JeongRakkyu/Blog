fcc644a68e33fd22dad70dc7ac3d9bac1adc2425
JeongRakkyu <JeongRakkyu@users.noreply.github.com>
2018-11-27 23:36:41 +0900

    [0.2.0] Separate php files. [Editor] JeongRakkyu [Desc] HTML files just include php files. PHP files are class.

---
+++ php/main.php
@@ -0,0 +1,33 @@
+<?php
+    class Main {
+        private $idx;
+        private $query;
+        private $recent;
+        private $subject;
+        private $content;
+        private $date;
+
+        function __construct($idx) {
+            $this->idx = $idx;
+            if (isset($this->idx)) {
+                include ('X');
+                $this->query = "SELECT X FROM X WHERE X = '$this->idx'";
+                $this->recent = $sql_connection->query($this->query)->fetch_assoc();
+                $sql_connection->close();
+            } else {
+                include ('X');
+                $this->query = "SELECT X FROM X WHERE X = 'javascript' ORDER BY X DESC";
+                $this->recent = $sql_connection->query($this->query)->fetch_assoc();
+                $sql_connection->close();
+            }
+            $this->subject = $this->recent['X'];
+            $this->content = $this->recent['X'];
+            $this->date = $this->recent['X'];
+        }
+
+        function load() {
+            echo "<div id='subject'><p>$this->subject</p><p>$this->date</p></div>";
+            echo "<div id='content'>$this->content</div>";
+        }
+    }
+?>
