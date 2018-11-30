9f800e7a068af292a9de566ef90178faa9840279
JeongRakkyu <JeongRakkyu@users.noreply.github.com>
2018-11-30 14:07:10 +0900

    [0.3.0] Github commit heatmap load. [Editor] JeongRakkyu [Desc] Commit heatmap load.
    Get contents from URL -> explode -> slice for getting what I want
    -> encode array to json -> echo.
    
    Using AJAX, get the array, and parse.

--- test.php
+++ test.php
@@ -1,6 +1,9 @@
 <?php
     $aa = file_get_contents('https://github.com/JeongRakkyu');
     $aa = explode("\n", $aa);
-    $aa = json_encode($aa);
-    echo "<script>var aa = $aa;</script>";
+    $bb = array_slice($aa, 707, 478);
+    $cc = array_slice($aa, 1204, 2);
+    $dd = array_merge($bb, $cc);
+    $dd = json_encode($dd);
+    echo $dd;
 ?>
